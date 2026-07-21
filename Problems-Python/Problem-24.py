636cf420300f7349bef63241628505cd53707ff2
AbdulManan30 <abdul.manan232332@gmail.com>
2026-07-21 06:13:04 +0500

    Problem 24 is solved

---
+++ Problems-Python/Problem-24.py
@@ -0,0 +1,21 @@
+'''
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+'''
+
+def intersection(num1, num2):
+    set1 = set(num1)
+    set2 = set(num2)
+    result = set1.intersection(set2)
+    return list(result)
